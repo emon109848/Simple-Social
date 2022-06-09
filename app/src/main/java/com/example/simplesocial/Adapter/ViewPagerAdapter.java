1c92ddcdfe07d90267d18ae3cc058847b0e52e5f
emon109848 <emon109848@gmail.com>
2022-06-09 22:10:10 +0600

    Version 1 Working on UI 2:11:46

--- app/src/main/java/com/example/simplesocial/Adapter/ViewPagerAdapter.java
+++ app/src/main/java/com/example/simplesocial/Adapter/ViewPagerAdapter.java
@@ -1,9 +1,13 @@
 package com.example.simplesocial.Adapter;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.simplesocial.Fragment.Notification2Fragment;
+import com.example.simplesocial.Fragment.RequestFragment;
 
 public class ViewPagerAdapter extends FragmentPagerAdapter {
     public ViewPagerAdapter(@NonNull FragmentManager fm) {
@@ -13,11 +17,28 @@ public class ViewPagerAdapter extends FragmentPagerAdapter {
     @NonNull
     @Override
     public Fragment getItem(int position) {
-        return null;
+        switch (position){
+            case 0: return new Notification2Fragment();
+            case 1: return new RequestFragment();
+            default:return new Notification2Fragment();
+        }
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return 2;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+
+        String title = null;
+        if(position==0){
+            title = "Notification";
+        }else if(position==1){
+            title = "Request";
+        }
+        return title;
     }
 }
